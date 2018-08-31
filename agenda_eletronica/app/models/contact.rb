9f0573fc797a9fcc747721a15144e9faba2af919
Rebeca Rancan <rebecarancanrr@gmail.com>
2018-08-31 09:13:31 -0300

    Consertando erro

--- agenda_eletronica/app/models/contact.rb
+++ agenda_eletronica/app/models/contact.rb
@@ -1,7 +1,7 @@
 class Contact < ApplicationRecord
   belongs_to :kind
   has_one :adress, dependent: :destroy
-  has_many :phones
+  has_many :phones, dependent: :destroy
 
   accepts_nested_attributes_for :adress
   accepts_nested_attributes_for :phones, reject_if: :all_blank, allow_destroy: true
--- agenda_eletronica/app/models/kind.rb
+++ agenda_eletronica/app/models/kind.rb
@@ -1,5 +1,5 @@
 class Kind < ApplicationRecord
-	has_many :contacts
+	has_many :contacts, dependent: :destroy
 
   validates :description, presence: true
 end
